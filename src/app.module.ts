b0bc545c01075ce576153b038757c723d4b8803c
muyeon <gmakin36@gmail.com>
2024-10-17 20:51:59 +0900

    add: job resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JobModule } from './job/job.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    JobModule,
   ],
   controllers: [AppController],
   providers: [AppService],
